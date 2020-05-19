c6df042267c1cf516ddc383d3560438c08ffc114
wAnto97 <antonino.geraci5@gmail.com>
2020-05-19 23:30:07 +0200

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,35 @@
+import json
+
+class utils():
+    def __init__(self):
+        pass
+
+    def writeOnFileLosses(self,file_path,group,losses):
+        training_loss = losses[0]
+        validation_loss = losses[1]
+
+        json_out = {}
+        json_out['group'+str(group)] = {}
+        json_out['group'+str(group)]['training_loss'] = training_loss
+        json_out['group'+str(group)]['validation_loss'] = validation_loss
+
+        with open(file_path,mode='a') as file_out:
+            json.dump(json_out,file_out)
+            file_out.write('\n')
+
+    def writeOnFileMetrics(self,file_path,group,metrics):
+        training_accuracy = metrics[0]
+        validation_accuracy = metrics[1]
+        test_accuracy = metrics[2]
+        classification_report = metrics[3]
+
+        json_out = {}
+        json_out['group'+str(group)] = {}
+        json_out['group'+str(group)]['training_accuracy'] = training_accuracy
+        json_out['group'+str(group)]['validation_accuracy'] = validation_accuracy
+        json_out['group'+str(group)]['test_accuracy'] = test_accuracy
+        json_out['group'+str(group)]['report'] = classification_report
+
+        with open(file_path,mode='a') as file_out:
+            json.dump(json_out,file_out)
+            file_out.write('\n')
