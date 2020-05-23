d2a878733fd85a81534391f8a06cf726792bb6e5
MatteoT9890 <53305467+MatteoT9890@users.noreply.github.com>
2020-05-23 21:00:59 +0200

    Update MyNet.py

--- MyNet.py
+++ MyNet.py
@@ -1,35 +1,27 @@
-class Utils():
-    def __init__(self):
-        pass
+from IncrementalLeraningMLDL.resnet_cifar import resnet32
+import torch
+from torch import nn
+import copy
 
-    def create_dataloaders(self,training_set,test_set,group,BATCH_SIZE):
-        train,val = training_set.get_single_train_joint_validation(group)
-        test = test_set.get_groups_joint(group)
-        
-            
-        train_dataloader =  DataLoader(train,batch_size=BATCH_SIZE,drop_last=True,num_workers=4,shuffle=True)
-        val_dataloader = DataLoader(val,batch_size=BATCH_SIZE,drop_last=False,num_workers=4)
-        test_dataloader = DataLoader(test,batch_size=BATCH_SIZE,drop_last=False,num_workers=4)
 
-        return train_dataloader,val_dataloader,test_dataloader
+class MyNet():
+    def __init__(self,n_classes):
+        self.net = resnet32()
+        self.net.linear = nn.Linear(64,n_classes)
+        self.init_weights = torch.nn.init.kaiming_normal_(self.net.linear.weight)
     
-    def create_onehot(self,intLabel,num_classes):
-        onehot = torch.zeros(num_classes)
-        onehot[intLabel]=1
-        return onehot.cuda()
+    def update_network(self,best_net,n_classes,init_weights):
+        prev_net = copy.deepcopy(best_net)
+        n_old_classes = best_net.linear.weight.shape[0]
+        prev_weights = copy.deepcopy(best_net.linear.weight)
+        self.net.linear = nn.Linear(64,n_classes)
+        self.net.linear.weight.data = torch.cat((prev_weights,init_weights))
 
-    def one_hot_matrix(self,labels,n_classes):
-        matrix = torch.zeros((len(labels),n_classes))
-        for index,y in enumerate(labels):
-            matrix[index] = self.create_onehot(y,n_classes)
-        return matrix.cuda()
+        return prev_net,self.net
 
-    def create_images_indices(self,dataloader,step,n_classes=10):
-        images_indices=[]
-        for i in range(n_classes):
-            images_indices.append([])
-        for images,labels,indices in dataloader:
-            for img,label,index in zip(images,labels,indices):
-                images_indices[label-(step-1)*n_classes].append((img,index))
+    def prepare_training(self,LR,MOMENTUM,WEIGHT_DECAY,STEP_SIZE,GAMMA):    
+        parameters_to_optimize = self.net.parameters()
+        optimizer = optim.SGD(parameters_to_optimize, lr=LR, momentum=MOMENTUM, weight_decay=WEIGHT_DECAY)
+        scheduler = optim.lr_scheduler.MultiStepLR(optimizer, STEP_SIZE, gamma=GAMMA)
 
-        return images_indices
+        return (optimizer,scheduler)
