e4f84739add2ebb25f6ebe3da35c6c92785ef0fc
MatteoT9890 <53305467+MatteoT9890@users.noreply.github.com>
2020-05-23 20:57:13 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,7 +1,9 @@
 import json
 import yaml
+import torch
+from torch.utils.data import  DataLoader
 
-class utils():
+class Utils():
     def __init__(self):
         pass
 
@@ -67,3 +69,35 @@ class utils():
             return accuracy_train_per_group,accuracy_val_per_group,accuracy_test_per_group,report_per_group
 
         return accuracy_train_per_group,accuracy_val_per_group,accuracy_test_per_group
+
+    def create_dataloaders(self,training_set,test_set,group,BATCH_SIZE):
+        train,val = training_set.get_single_train_joint_validation(group)
+        test = test_set.get_groups_joint(group)
+        
+            
+        train_dataloader =  DataLoader(train,batch_size=BATCH_SIZE,drop_last=True,num_workers=4,shuffle=True)
+        val_dataloader = DataLoader(val,batch_size=BATCH_SIZE,drop_last=False,num_workers=4)
+        test_dataloader = DataLoader(test,batch_size=BATCH_SIZE,drop_last=False,num_workers=4)
+
+        return train_dataloader,val_dataloader,test_dataloader
+    
+    def create_onehot(self,intLabel,num_classes):
+        onehot = torch.zeros(num_classes)
+        onehot[intLabel]=1
+        return onehot.cuda()
+
+    def one_hot_matrix(self,labels,n_classes):
+        matrix = torch.zeros((len(labels),n_classes))
+        for index,y in enumerate(labels):
+            matrix[index] = self.create_onehot(y,n_classes)
+        return matrix.cuda()
+
+    def create_images_indices(self,dataloader,step,n_classes=10):
+        images_indices=[]
+        for i in range(n_classes):
+            images_indices.append([])
+        for images,labels,indices in dataloader:
+            for img,label,index in zip(images,labels,indices):
+                images_indices[label-(step-1)*n_classes].append((img,index))
+
+        return images_indices
