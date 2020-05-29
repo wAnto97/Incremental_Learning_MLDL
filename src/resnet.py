b81cd728fdde3b2fcf376b610544137e78cb2666
wAnto97 <antonino.geraci5@gmail.com>
2020-05-29 10:29:31 +0200

    Update resnet.py

--- src/resnet.py
+++ src/resnet.py
@@ -97,7 +97,7 @@ class ResNet(nn.Module):
         self.layer2 = self._make_layer(block, 32, layers[1], stride=2)
         self.layer3 = self._make_layer(block, 64, layers[2], stride=2)
         self.avgpool = nn.AvgPool2d(8, stride=1)
-        self.fc = nn.Linear(64 * block.expansion, num_classes)
+        self.linear = nn.Linear(64 * block.expansion, num_classes)
         self.out_dim = 64 * block.expansion
 
         for m in self.modules():
@@ -135,7 +135,7 @@ class ResNet(nn.Module):
 
         x = self.avgpool(x)
         x = x.view(x.size(0), -1)
-        x = self.fc(x)
+        x = self.linear(x)
 
         return x
 
