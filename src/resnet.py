692d993bb2ddfeea42dbe3464a887a0ad69dbe6c
wAnto97 <antonino.geraci5@gmail.com>
2020-06-01 09:01:34 +0200

    Update resnet.py

--- src/resnet.py
+++ src/resnet.py
@@ -96,7 +96,7 @@ class ResNet(nn.Module):
         self.relu = nn.ReLU(inplace=True)
         self.layer1 = self._make_layer(block, 16, layers[0])
         self.layer2 = self._make_layer(block, 32, layers[1], stride=2)
-        self.layer3 = self._make_layer(block, 64, layers[2], stride=2)
+        self.layer3 = self._make_layer(block, 64, layers[2], stride=2,relu=False)
         self.avgpool = nn.AvgPool2d(8, stride=1)
         self.linear = nn.Linear(64 * block.expansion, num_classes)
         self.out_dim = 64 * block.expansion
@@ -108,14 +108,21 @@ class ResNet(nn.Module):
                 nn.init.constant_(m.weight, 1)
                 nn.init.constant_(m.bias, 0)
 
-    def _make_layer(self, block, planes, blocks, stride=1):
+    def _make_layer(self, block, planes, blocks, stride=1,relu=True):
         downsample = None
         if stride != 1 or self.inplanes != planes * block.expansion:
-            downsample = nn.Sequential(
-                nn.Conv2d(self.inplanes, planes * block.expansion,
-                          kernel_size=1, stride=stride, bias=False),
-                nn.BatchNorm2d(planes * block.expansion),
-            )
+            if(relu == True):
+                downsample = nn.Sequential(
+                    nn.Conv2d(self.inplanes, planes * block.expansion,
+                            kernel_size=1, stride=stride, bias=False),
+                    nn.BatchNorm2d(planes * block.expansion),
+                )
+            else:
+                downsample = nn.Sequential(
+                    nn.Conv2d(self.inplanes, planes * block.expansion,
+                            kernel_size=1, stride=stride, bias=False),
+                )
+
 
         layers = []
         layers.append(block(self.inplanes, planes, stride, downsample))
@@ -126,7 +133,7 @@ class ResNet(nn.Module):
         return nn.Sequential(*layers)
 
     def forward(self, x):
-        x = self.conv1(x)
+       x = self.conv1(x)
         x = self.bn1(x)
         x = self.relu(x)
 
@@ -136,7 +143,9 @@ class ResNet(nn.Module):
 
         x = self.avgpool(x)
         x = x.view(x.size(0), -1)
-        x = self.linear(x)
+        x = x/x.norm()
+        norm_weights = (self.linear.weight.data/self.linear.weight.data.norm()).cuda()
+        x = F.linear(x,norm_weights,self.linear.bias.data/self.linear.bias.data.norm())
 
         return x
 
