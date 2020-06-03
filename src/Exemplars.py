5b34f2dfe8d3bc06e6994a365480a2d09a37ecdb
MatteoT9890 <53305467+MatteoT9890@users.noreply.github.com>
2020-06-03 23:15:50 +0200

    Update Exemplars.py

--- src/Exemplars.py
+++ src/Exemplars.py
@@ -3,7 +3,7 @@ import torch
 import numpy as np
 
 class Exemplars():
-    def __init___(self,K=2000):
+    def __init___(self,K):
         self.exemplar_set=[]
         self.exemplar_centroids=[]
         self.K=K
