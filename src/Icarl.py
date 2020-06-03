7bf202aa402f85213c61a03bbcb215f981b71699
MatteoT9890 <53305467+MatteoT9890@users.noreply.github.com>
2020-06-03 23:14:52 +0200

    Update Icarl.py

--- src/Icarl.py
+++ src/Icarl.py
@@ -6,7 +6,7 @@ from IncrementalLeraningMLDL.src.Exemplars import Exemplars
 
 class Icarl(Exemplars):
     def __init__(self,K=2000):
-       super(Icarl,self).__init__(K)
+       super().__init__(K)
        
     def predict(self,images,net):
         """
