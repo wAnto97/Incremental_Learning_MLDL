7c2327fdbe50693d7d06faa550ea6e5cd770f5ea
MatteoT9890 <53305467+MatteoT9890@users.noreply.github.com>
2020-06-05 17:03:39 +0200

    Update LfC.py

--- src/LfC.py
+++ src/LfC.py
@@ -2,4 +2,4 @@ from IncrementalLeraningMLDL.src.Exemplars import Exemplars
 
 class LfC(Exemplars):
     def __init__(self,K=2000):
-       super(LfG,self).__init__(K)
+       super(LfC,self).__init__(K)
