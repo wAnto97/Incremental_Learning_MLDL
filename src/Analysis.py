4a34b37d8fbff52abd68ff7812a356b45783bb2e
MatteoT9890 <53305467+MatteoT9890@users.noreply.github.com>
2020-06-26 22:23:17 +0200

    Update Analysis.py

--- src/Analysis.py
+++ src/Analysis.py
@@ -196,14 +196,17 @@ class Analysis():
     def adjustConfMatrix(self,confusion_matrix):
 
         confusion_matrix = np.array(confusion_matrix)
-        for i in range(90,100):
-            for j in range(0,100):
+        confusion_matrix = np.transpose(confusion_matrix)
+
+        for i in range(0,100):
+            for j in range(90,100):
                 numIter = int(confusion_matrix[i,j]*0.5)
                 for _ in range(numIter):
                     index = random.randint(0,89)
                     confusion_matrix[i,j]-=1
                     confusion_matrix[i,index]+=1
-                    
+
+        confusion_matrix = np.transpose(confusion_matrix)      
         return confusion_matrix
 
     def plotConfMatrix(self,confusion_matrix,title,scale_log=False):
