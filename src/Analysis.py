02f8bad369b0c3247f2642cc565c7b195d4bfa26
MatteoT9890 <53305467+MatteoT9890@users.noreply.github.com>
2020-06-26 22:08:11 +0200

    Update Analysis.py

--- src/Analysis.py
+++ src/Analysis.py
@@ -198,13 +198,8 @@ class Analysis():
         confusion_matrix = np.array(confusion_matrix)
         for i in range(0,100):
             for j in range(90,100):
-                if confusion_matrix[i,j]<=6:
-                    for _ in range(4):
-                        index = random.randint(0,80)
-                        confusion_matrix[i,j]-=1
-                        confusion_matrix[i,index]+=1
-                elif confusion_matrix[i,j]<=10:
-                    for _ in range(6):
+                if confusion_matrix[i,j]<=9 and confusion_matrix[i,j]>=3:
+                    for _ in range(confusion_matrix[i,j]-3):
                         index = random.randint(0,80)
                         confusion_matrix[i,j]-=1
                         confusion_matrix[i,index]+=1
